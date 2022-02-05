bbc368e90479cf0a0b9184bfb776405bede925c4
truckDriverDev <hungrywolfkang@gmail.com>
2022-02-04 23:11:27 -0500

    update and clean code

--- main.js
+++ main.js
@@ -2,23 +2,17 @@ const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const file = document.getElementById('fileupload');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let audioSource;
 let analyser;
-
 const audioCtx = new AudioContext();
-
-//drawText();
-
 const audio1 = document.getElementById('audio1');
 
+//audio file upload
 file.addEventListener('change', function(){
-    const files = this.files;
-        
-    audio1.src = URL.createObjectURL(files[0]);
-   
+    const files = this.files;        
+    audio1.src = URL.createObjectURL(files[0]);   
     audioSource = audioCtx.createMediaElementSource(audio1);
     analyser = audioCtx.createAnalyser();
     audioSource.connect(analyser);
@@ -26,25 +20,21 @@ file.addEventListener('change', function(){
     analyser.fftSize = 256;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-
     const barWidth = 5;
     let barHeight;
     let x;
 
     function animate(){
-        x = 0;
-       
+        x = 0;       
         analyser.getByteFrequencyData(dataArray);
         drawVisualizer(bufferLength, x, barWidth /2, barHeight / 2, dataArray);
-        //drawText();
         requestAnimationFrame(animate);
-    }
-  
+    }  
     animate();
 });
 
-function drawVisualizer(bufferLength, x, barWidth, barHeight,dataArray){
-   
+//visualization
+function drawVisualizer(bufferLength, x, barWidth, barHeight,dataArray){   
     for(let i = 0; i < bufferLength; i++){
         barHeight = dataArray[i];
         ctx.save();
@@ -54,17 +44,14 @@ function drawVisualizer(bufferLength, x, barWidth, barHeight,dataArray){
         let y = Math.cos(i * Math.PI / 180) + 90;
         ctx.translate(canvas.width / 2 + x * 3, canvas.height  * 2 / 3);
         ctx.rotate(i + Math.PI * 4 / bufferLength);
-
         const hue2 = i * 0.9 + 280;
         const hue = i / bufferLength  * 20;
         ctx.fillStyle = 'hsl('+ hue2 +', 100%, 70%)';
         ctx.strokeStyle = 'hsl('+ hue2 +', 100%, 50%)';
-
         ctx.shadowOffsetX = 10;
         ctx.shadowOffsetY = 10;
         ctx.shadowBlur = 8;
         ctx.shadowColor = 'rgba(0, 0, 0, 0.1)';
-
         ctx.globalCompositeOperation = 'source-over';    
 
         //Circle
@@ -73,30 +60,14 @@ function drawVisualizer(bufferLength, x, barWidth, barHeight,dataArray){
         ctx.arc(0, barHeight / 2, barHeight / 15, 0, Math.PI * 4);
         ctx.fill();
         ctx.lineWidth = 10;
-        ctx.strokeStyle =  'hsl(' + hue +', 100%, 90%)';
-    
+        ctx.strokeStyle =  'hsl(' + hue +', 100%, 90%)';    
         ctx.restore();
         x += barWidth;
     }
 };
 
-// function drawText(){       
-//     ctx.fillStyle = 'white';     
-//     ctx.font = '25px Ariel';
-//     ctx.fillText('Spiritual Awakening', 60, 50);
-//     ctx.font = '20px Ariel';
-//     ctx.fillText('"The ability of discovering who you are."', 60, 90);
-//     ctx.font = '15px Ariel';
-  
-//     ctx.fillText('Music Visualizer Created by Wolfkang Lim', canvas.width * 2 / 3 , canvas.height * 0.98);
-// }
-
-//canvs to webm video file download
-//another from pretagteam
-
 const recordBtn = document.getElementById("recordBtn");
 const stopBtn = document.getElementById("stopBtn");
-
 let recording = false;
 let mediaRecorder;
 let recordedChunks;
@@ -105,16 +76,13 @@ recordBtn.addEventListener("click", () => {
    let dest = audioCtx.createMediaStreamDestination();
    audioSource.connect(dest);
    audioSource.connect(audioCtx.destination);
-   let audioTrack = dest.stream.getAudioTracks()[0];
-   
+   let audioTrack = dest.stream.getAudioTracks()[0];   
    recording = !recording;
    if (recording) {
       recordBtn.style.backgroundColor = "#aaa";
       stopBtn.style.backgroundColor = 'blue';
-
       const canvasStream = canvas.captureStream(60);
-      //video.srcObject = canvasStream;
-    
+      //video.srcObject = canvasStream;    
       mediaRecorder = new MediaRecorder(canvasStream, {
         mimeType: 'video/webm;codecs=vp9'        
       }); 
